b5270cb4a129e3f564fb749d011a8ea13ac6bbfc
NadirZenith <2cb.md2@gmail.com>
2022-02-15 00:04:01 +0100

    added unique product name

--- php/srv/src/Entity/Product.php
+++ php/srv/src/Entity/Product.php
@@ -5,9 +5,11 @@ namespace App\Entity;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
+ * @UniqueEntity("name")
  */
 class Product
 {
